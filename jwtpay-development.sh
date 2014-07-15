d1fa8df321b1658634f16c85c6ebda3f7587f540
Tuan Le Minh <tuanlm.101@gmail.com>
2014-07-15 12:10:07 +0700

    test

---
+++ jwtpay-development.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+forever stop $PWD/src/jwtpay.js
+forever start -a -l $PWD/log/jwtpay/log.txt -o $PWD/log/jwtpay/out.txt -e $PWD/log/jwtpay/err.txt -w --watchDirectory $PWD/src $PWD/src/jwtpay.js
